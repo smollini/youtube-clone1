befc2e217ad4a8fce8e5691d7c7141de2f32aa90
mikolaj <smolinskim44@gmail.com>
2022-03-11 18:22:12 +0100

    ew

--- backend/src/main/java/com/programming/techie/youtubeclone/controller/VideoController.java
+++ backend/src/main/java/com/programming/techie/youtubeclone/controller/VideoController.java
@@ -3,15 +3,10 @@ package com.programming.techie.youtubeclone.controller;
 
 import com.programming.techie.youtubeclone.service.VideoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.List;
 
 @RestController
 @RequestMapping(path= "api/videos")
